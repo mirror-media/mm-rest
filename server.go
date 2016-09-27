2d4b7947401daf6688f220dc477f7b5071534a8e
ChienHsin-Chan <hcchien@gmail.com>
2016-09-27 16:49:56 +0800

    add the redis package for conn pool

--- server.go
+++ server.go
@@ -2,65 +2,34 @@ package main
 
 import (
 	"flag"
+	"log"
 
-	"github.com/garyburd/redigo/redis"
-	"github.com/gin-gonic/gin"
+	"github.com/mirror-media/mm-rest"
 )
 
 var (
-	redisAddress   = flag.String("redis-address", ":6379", "Address to the Redis server")
-	maxConnections = flag.Int("max-connections", 10, "Max connections to Redis")
+	redisAddress = flag.String("redis-address", ":6379", "Address to the Redis server")
+	redisAuth    = flag.String("redis-auth", "", "Password to the Redis server")
 )
 
 func main() {
+	flag.Parse()
 	router := gin.Default()
-	router.GET("/ready", checkRedis)
-	router.GET("/ready/:auth", checkRedisWithAuth)
-
-	router.Run()
-}
-
-func checkRedisWithAuth(c *gin.Context) {
-	auth := c.Param("auth")
-	doRedis(c, "PING", auth)
-}
-
-func checkRedis(c *gin.Context) {
-	doRedis(c, "PING", "")
-}
-
-func doRedis(c *gin.Context, cmd, auth string) {
-	//client, err := redis.Dial("tcp", *redisAddress)
-	redisPool := redis.NewPool(func() (redis.Conn, error) {
-		client, err := redis.Dial("tcp", *redisAddress)
+	log.Printf("redis address is %s\n", *redisAddress)
+	log.Printf("redis auth is %s\n", *redisAuth)
+	redis := gingo.NewRedisStore(*redisAddress, *redisAuth)
+	router.GET("/ready", func(c *gin.Context) {
+		ret, err := redis.Do("PING")
 		if err != nil {
 			c.JSON(500, gin.H{
 				"message": err,
 			})
-		}
-		return client, err
-	}, *maxConnections)
-
-	defer redisPool.Close()
-
-	r := redisPool.Get()
-	defer r.Close()
-
-	if auth != "" {
-		if _, err := r.Do("AUTH", auth); err != nil {
-			c.JSON(400, gin.H{
-				"message": "Invalid password",
-			})
 			return
 		}
-	}
-	ret, err := r.Do(cmd)
-	if err != nil {
-		c.JSON(500, gin.H{
-			"message": err,
+		c.JSON(200, gin.H{
+			"message": ret,
 		})
-	}
-	c.JSON(200, gin.H{
-		"message": ret,
 	})
+
+	router.Run()
 }
