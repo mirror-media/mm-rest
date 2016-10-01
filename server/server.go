3eb781c276c64830148ebf9b3d779b3db5a55905
ChienHsin-Chan <hcchien@gmail.com>
2016-10-01 22:59:50 +0800

    use mset and mgetall

--- server/server.go
+++ server/server.go
@@ -4,6 +4,7 @@ import (
 	"crypto/md5"
 	"encoding/hex"
 	"flag"
+	"fmt"
 	"log"
 
 	"github.com/gin-gonic/gin"
@@ -12,9 +13,9 @@ import (
 
 var (
 	redisAddress = flag.String("redis-address", ":6379", "Address to the Redis server")
+	redisPrimary = flag.String("redis-primary", ":6379", "Address to the Redis Primary server")
 	redisAuth    = flag.String("redis-auth", "", "Password to the Redis server")
 	name         = ""
-	email        = ""
 	key          = ""
 )
 
@@ -23,6 +24,7 @@ func main() {
 	router := gin.Default()
 	log.Printf("redis address is %s\n", *redisAddress)
 	log.Printf("redis auth is %s\n", *redisAuth)
+	redisPrimary := gingo.NewRedisStore(*redisPrimary, *redisAuth)
 	redis := gingo.NewRedisStore(*redisAddress, *redisAuth)
 	router.GET("/ready", func(c *gin.Context) {
 		ret, err := redis.Do("PING")
@@ -39,8 +41,10 @@ func main() {
 	router.GET("/check", func(c *gin.Context) {
 		hasher := md5.New()
 		name := c.Query("name")
-		email := c.Query("email")
-		hasher.Write([]byte(name + email))
+		q1 := c.Query("q1")
+		q3 := c.Query("q3")
+		q4 := c.Query("q4")
+		hasher.Write([]byte(name))
 		redis_key := hex.EncodeToString(hasher.Sum(nil))
 		log.Printf("redis key is %s\n", redis_key)
 		ret, err := redis.Do("EXISTS", redis_key)
@@ -50,9 +54,31 @@ func main() {
 			})
 			return
 		}
+		ret, err = redis.Do("HGETALL", "listing-form")
+		switch v := ret.(type) {
+		case string:
+			fmt.Println(v)
+		case int, uint, int32, int64:
+			fmt.Println(v)
+		case byte:
+			fmt.Println("byte")
+		default:
+
+		}
+		//resp := "{ \"total\": " + total + ", \"q1r\": \"" + q1r + ", \"q3r\": " + q3r + ", \"q4r\": " + q4r + "}"
 		c.JSON(200, gin.H{
 			"result": ret,
 		})
+		redisPrimary.Do("HINCRBY", "listing-form", "total", 1)
+		if q1 == "1" {
+			redisPrimary.Do("HINCRBY", "listing-form", "q1r", 1)
+		}
+		if q3 == "1" {
+			redisPrimary.Do("HINCRBY", "listing-form", "q3r", 1)
+		}
+		if q4 == "1" {
+			redisPrimary.Do("HINCRBY", "listing-form", "q4r", 1)
+		}
 	})
 
 	router.Run()
